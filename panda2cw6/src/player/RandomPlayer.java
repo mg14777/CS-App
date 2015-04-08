abf15bac931db96321b75e21e0a7ef482d94ab1c
mg14777 <mg14777@my.bristol.ac.uk>
2015-04-08 12:26:59 +0100

    Update RandomPlayer.java

--- panda2cw6/src/player/RandomPlayer.java
+++ panda2cw6/src/player/RandomPlayer.java
@@ -3,7 +3,11 @@ package player;
 import scotlandyard.Move;
 import scotlandyard.Player;
 import scotlandyard.ScotlandYardView;
-
+import scotlandyard.ScotlandYardGraphReader;
+import scotlandyard.*;
+import scotlandyard.Graph;
+import scotlandyard.Colour;
+import scotlandyard.MoveTicket;
 import java.util.Random;
 import java.util.Set;
 
@@ -16,11 +20,37 @@ import java.util.Set;
  * which must be one from the list.
  */
 public class RandomPlayer implements Player {
-    //public score () {
-        
-    //}
+    ScotlandYardView view;
+    private Graph map;
+    public int score (Set<Move> move) {
+            int score = 0;
+            int danger = 0;
+            final int mapCorner = 5;
+            final int mapMain = 10;
+            final int movePoint = 2;
+            score = score + (move.size()*2);
+            int neighbours = map.getEdges(view.getPlayerLocation(Colour.Black)).size();
+            if(neighbours < 4)
+            	score += mapCorner;
+            else
+            	score += mapMain;
+            /*for(Colour player: view.getPlayers()) {
+                for(Move singleMove: move) 
+                    if(singleMove instanceof MoveTicket)
+                        if(singleMove.target == view.getPlayerLocation(player))
+                            danger++;
+            }
+            */
+            
+            return score;
+            
+    }
     public RandomPlayer(ScotlandYardView view, String graphFilename) {
         //TODO: A better AI makes use of `view` and `graphFilename`.
+        this.view = view;
+        ScotlandYardGraphReader read = new ScotlandYardGraphReader();
+        try {map = read.readGraph(graphFilename);}
+        catch(Exception e) { System.out.println("Inexistent file");}
     }
 
     @Override
