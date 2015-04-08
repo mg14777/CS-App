2a30dbd1811482bd9305c3488bb6e0050c152544
mg14777 <mg14777@my.bristol.ac.uk>
2015-04-08 22:25:49 +0100

    Update RandomPlayer.java

--- panda2cw6/src/player/RandomPlayer.java
+++ panda2cw6/src/player/RandomPlayer.java
@@ -1,13 +1,13 @@
 package player;
 
-import scotlandyard.Move;
-import scotlandyard.Player;
-import scotlandyard.ScotlandYardView;
-import scotlandyard.ScotlandYardGraphReader;
 import scotlandyard.*;
-import scotlandyard.Graph;
-import scotlandyard.Colour;
+import solution.ScotlandYardModel;
 import scotlandyard.MoveTicket;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Random;
 import java.util.Set;
 
@@ -21,14 +21,16 @@ import java.util.Set;
  */
 public class RandomPlayer implements Player {
     ScotlandYardView view;
+    String graphFilename;
     private Graph map;
-    public int score (Set<Move> move) {
+    List<RealPlayer> simulatedPlayers = new ArrayList<RealPlayer>();
+    public int score (List<Move> move) {
             int score = 0;
             int danger = 0;
             final int mapCorner = 5;
             final int mapMain = 10;
             final int movePoint = 2;
-            score = score + (move.size()*2);
+            score = score + (move.size()*movePoint);
             int neighbours = map.getEdges(view.getPlayerLocation(Colour.Black)).size();
             if(neighbours < 4)
             	score += mapCorner;
@@ -48,23 +50,83 @@ public class RandomPlayer implements Player {
     public RandomPlayer(ScotlandYardView view, String graphFilename) {
         //TODO: A better AI makes use of `view` and `graphFilename`.
         this.view = view;
+        this.graphFilename = graphFilename;
         ScotlandYardGraphReader read = new ScotlandYardGraphReader();
         try {map = read.readGraph(graphFilename);}
         catch(Exception e) { System.out.println("Inexistent file");}
     }
-
+    public void initialise() {
+    	for(Colour colour :view.getPlayers()) {
+    		Map<Ticket, Integer> tickets = new HashMap<Ticket, Integer>();
+    		if(colour.equals(Colour.Black)) {
+    			tickets.put(Ticket.Bus, view.getPlayerTickets(Colour.Black, Ticket.Bus));
+    			tickets.put(Ticket.Taxi, view.getPlayerTickets(Colour.Black, Ticket.Taxi));
+    			tickets.put(Ticket.Underground, view.getPlayerTickets(Colour.Black, Ticket.Underground));
+    			tickets.put(Ticket.Double, view.getPlayerTickets(Colour.Black, Ticket.Double));
+    			tickets.put(Ticket.Secret, view.getPlayerTickets(Colour.Black, Ticket.Secret));
+    		}
+    		else {
+    			tickets.put(Ticket.Bus, view.getPlayerTickets(colour, Ticket.Bus));
+    			tickets.put(Ticket.Taxi, view.getPlayerTickets(colour, Ticket.Taxi));
+    			tickets.put(Ticket.Underground, view.getPlayerTickets(colour, Ticket.Underground));
+    		}
+    		simulatedPlayers.add(new RealPlayer(colour,view.getPlayerLocation(colour),tickets));
+    	}
+    	
+    }
+    public void modify(Colour colour,Move move) {
+    	for(Colour colour1: view.getPlayers()) {
+    		if(move instanceof MoveDouble) {
+				getPlayer(colour).location = ((MoveDouble) move).move2.target;
+				int valdb = getPlayer(colour).tickets.get(Ticket.Double);
+				getPlayer(colour).tickets.put(Ticket.Double, valdb-1);
+				int val1 = getPlayer(colour).tickets.get(((MoveDouble) move).move1.ticket);
+				getPlayer(colour).tickets.put(((MoveDouble) move).move1.ticket, val1-1);
+				int val2 = getPlayer(colour).tickets.get(((MoveDouble) move).move2.ticket);
+				getPlayer(colour).tickets.put(((MoveDouble) move).move2.ticket, val2-1);
+			}
+    		else if(move instanceof MoveTicket) {
+    			getPlayer(colour).location = ((MoveTicket) move).target;
+    			int val = getPlayer(colour).tickets.get(((MoveTicket) move).ticket);
+    			getPlayer(colour).tickets.put(((MoveTicket) move).ticket, val-1);
+    		}
+    		else
+    			;
+    	}
+    			
+    		
+    }
+    public RealPlayer getPlayer(Colour colour) {
+        for(RealPlayer player: simulatedPlayers)
+            if(player.colour.equals(colour))
+                return player;
+        return null;
+    }
     @Override
     public Move notify(int location, Set<Move> moves) {
         //TODO: Some clever AI here ...
-
-        int choice = new Random().nextInt(moves.size());
+    	int score = 0;
+    	Move selectMove = MoveTicket.instance(Colour.Black,Ticket.Bus, 0);
+    	initialise();
+    	for(Move move : moves) {
+    		modify(Colour.Black,move);
+    		Model testModel = new Model(view,simulatedPlayers,graphFilename,view.getRounds());
+    		int newScore = score(testModel.validMoves(Colour.Black));
+    		if(newScore > score) {
+    		    selectMove = move;
+    			score = newScore;
+    		}
+    	}
+        /*int choice = new Random().nextInt(moves.size());
         for (Move move : moves) {
             if (choice == 0) {
                 return move;
             }
             choice--;
-        }
-
-        return null;
+        }*/
+        return selectMove;
     }
+    
+    
+    	    
 }
