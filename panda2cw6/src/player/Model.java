b246a791ea5acab094d1924abe7efae01e7a50a8
mg14777 <mg14777@my.bristol.ac.uk>
2015-04-08 22:27:08 +0100

    Create Model.java

---
+++ panda2cw6/src/player/Model.java
@@ -0,0 +1,178 @@
+package player;
+import java.util.*;
+
+import scotlandyard.*;
+
+
+
+public class Model {
+	//int mrxLocation;
+	ScotlandYardView view;
+	//Map<Ticket,Integer> mrxTickets;
+	private List<RealPlayer> players = new ArrayList<RealPlayer>();
+	private Graph map;
+	 private List<Boolean> rounds;
+	Model(ScotlandYardView view,List<RealPlayer> players,String graphFileName,List<Boolean> rounds) {
+		this.view = view;
+		this.players = players;
+		this.rounds = rounds;
+		ScotlandYardGraphReader read = new ScotlandYardGraphReader();
+		try {map = read.readGraph(graphFileName);}
+        catch(Exception e) { System.out.println("Inexistent file");}
+	}
+	
+	
+	protected boolean check(Colour colour,int target) {
+        for(RealPlayer player : players) {
+            if(!player.colour.equals(colour))
+                if((player.location ==  target) && (!player.colour.equals(Colour.Black))) {
+                    return false;
+                }
+        }
+        return true;
+    }
+	
+	/**
+     * 
+     * @param colour Colour of Player
+     * @param target1 Location for Move 1
+     * @param target2 Location for Move 2
+     * @param route1  Transport for Move 1
+     * @param route2  Transport for Move 2
+     * @param secretmoves No. of Secret Move Tickets
+     * @return Creates double secret moves for MrX
+     */
+    protected List<Move> doublesecretMoves (Colour colour, int target1, int target2,
+     Edge<Integer,Route> route1, Edge<Integer,Route> route2, int secretmoves) {
+        MoveTicket move1 = MoveTicket.instance(colour,Ticket.fromRoute(route1.data()),target1);
+        MoveTicket move2 = MoveTicket.instance(colour,Ticket.fromRoute(route2.data()),target2);
+        MoveTicket move1secret = MoveTicket.instance(colour,Ticket.Secret,target1);
+        MoveTicket move2secret = MoveTicket.instance(colour,Ticket.Secret,target2);
+        List<Move> doublesecretmoves = new ArrayList<Move>();
+        if(secretmoves!=0) {
+            doublesecretmoves.add(MoveDouble.instance(colour,move1secret,move2));
+            doublesecretmoves.add(MoveDouble.instance(colour,move1,move2secret));
+        }
+        if(secretmoves>1)
+            doublesecretmoves.add(MoveDouble.instance(colour,move1secret,move2secret));
+        return doublesecretmoves;
+    }
+    /**
+     * 
+     * @param colour Colour of Player
+     * @param target Target location of Move
+     * @param route1 Transport of Move
+     * @param secretmoves No. of Secret Move Tickets
+     * @return All the possible double moves for MrX
+     */
+    protected List<Move> doublevalidMoves(Colour colour, int target,
+     Edge<Integer,Route> route1, int secretmoves) {
+        List<Edge<Integer, Route>> routes2 = new ArrayList<Edge<Integer,Route>>(map.getEdges(target));
+        List<Move> doublemoves = new ArrayList<Move>();
+        for(Edge<Integer,Route> route2 : routes2 ) {
+            MoveTicket move1 = MoveTicket.instance(colour,Ticket.fromRoute(route1.data()),target);
+            if(route1.data().equals(route2.data())) {
+                if(getPlayerTickets(colour,Ticket.fromRoute(route2.data())) > 1) {
+                    if((route2.target()).equals(target)) {
+                        MoveTicket move2 = MoveTicket.instance(colour, Ticket.fromRoute(route2.data()), route2.source());
+                        if(check(colour,route2.source())) {
+                            doublemoves.add(MoveDouble.instance(colour, move1, move2));
+                            doublemoves.addAll(doublesecretMoves(colour, target, route2.source(), route1, route2, secretmoves));
+                        }
+                    }
+                    else {
+                        MoveTicket move2 = MoveTicket.instance(colour,Ticket.fromRoute(route2.data()),route2.target());
+                        if(check(colour,route2.target())) {
+                            doublemoves.add(MoveDouble.instance(colour,move1,move2));
+                            doublemoves.addAll(doublesecretMoves(colour, target, route2.target(), route1, route2, secretmoves));
+                        }
+                    }
+                }
+            }
+            else {
+                if(getPlayerTickets(colour, Ticket.fromRoute(route2.data())) != 0) {
+                    if((route2.target()).equals(target)) {
+                        MoveTicket move2 = MoveTicket.instance(colour,Ticket.fromRoute(route2.data()), route2.source());
+                        if(check(colour, route2.source())) {
+                            doublemoves.add(MoveDouble.instance(colour,move1,move2));
+                            doublemoves.addAll(doublesecretMoves(colour,target,route2.source(), route1, route2, secretmoves));
+                        }
+                    }
+                    else {
+                        MoveTicket move2 = MoveTicket.instance(colour,Ticket.fromRoute(route2.data()),route2.target());
+                        if(check(colour, route2.target())) {
+                            doublemoves.add(MoveDouble.instance(colour, move1, move2));
+                            doublemoves.addAll(doublesecretMoves(colour, target, route2.target(), route1, route2, secretmoves));
+                        }
+                    }
+                }
+            }
+        }
+        return doublemoves;
+    }
+    
+   
+    public List<Move> validMoves(Colour player) {
+        int location = getPlayer(player).location;
+        int flag = 0;
+        if(player.equals(Colour.Black))
+            flag = 1;
+        List<Edge<Integer, Route>> routes = new ArrayList<Edge<Integer,Route>>(map.getEdges(location));
+        List<Move> moves = new ArrayList<Move>();
+        int doublemovetickets = getPlayerTickets(player,Ticket.Double);
+        int secretmovetickets = getPlayerTickets(player,Ticket.Secret);
+        for(Edge<Integer,Route> route : routes ) {
+            if(getPlayerTickets(player,Ticket.fromRoute(route.data())) != 0) {
+                if((route.target()).equals(location)) {
+                    if(check(player,route.source())) {
+                        moves.add(MoveTicket.instance(player,Ticket.fromRoute(route.data()), route.source()));
+                        if(flag==1 && secretmovetickets!=0)
+                            moves.add(MoveTicket.instance(player,Ticket.Secret, route.source()));
+                    }
+                    if(flag==1 && check(player,route.source()) && doublemovetickets!=0)
+                        moves.addAll(doublevalidMoves(player, route.source(), route, secretmovetickets));
+                }
+                else {
+                    if(check(player,route.target())) {
+                        moves.add(MoveTicket.instance(player,Ticket.fromRoute(route.data()),route.target()));
+                        if(flag==1 && secretmovetickets!=0)
+                            moves.add(MoveTicket.instance(player,Ticket.Secret,route.target()));
+                    }
+                    if(flag==1 && check(player,route.target()) && doublemovetickets!=0)
+                        moves.addAll(doublevalidMoves(player, route.target(), route, secretmovetickets));
+                }
+            }
+        }
+        if(moves.size() == 0&&!player.equals(Colour.Black))
+            moves.add(MovePass.instance(player));
+        return moves;
+    }
+    public int getPlayerLocation(Colour colour) {
+            for(RealPlayer player: players)
+                if(player.colour.equals(colour))
+                    return player.location;
+            return 0;
+    }
+    /**
+     * 
+     * @param colour Colour of the Player to be returned
+     * @return Specified player on basis of Colour
+     */
+    public RealPlayer getPlayer(Colour colour) {
+        for(RealPlayer player: players)
+            if(player.colour.equals(colour))
+                return player;
+        return null;
+    }
+    public int getPlayerTickets(Colour colour, Ticket ticket) {
+        for(RealPlayer player: players)
+            if(player.colour.equals(colour))
+                return player.tickets.get(ticket);
+        return 0;
+        
+    }
+	
+	
+	
+	
+}
